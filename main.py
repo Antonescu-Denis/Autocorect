f8816edbc40264362768a3b32d4808b2f89f9643
GarlicBread75 <Antonescu.Co.Denis@student.utcluj.ro>
2026-03-15 13:00:06 +0200

    .

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from pynput import keyboard as kb
-from pynput import mouse
 from pynput.keyboard import Key
 import wordfreq, Levenshtein
 
@@ -18,14 +17,6 @@ def key_press(key):
             k = '---'
         print(k)
 
-def mouse_click(x, y, button, pressed):
-    print(f"{'Pressed' if pressed else 'Released'}  at {(x, y)}")
-    if not pressed:
-        return False
-
 #kb_listen = kb.Listener(on_press = key_press)
 #kb_listen.start()
 #kb_listen.join()
-mouse_listen = mouse.Listener(on_click = mouse_click)
-mouse_listen.start()
-mouse_listen.join()
